b3f6c253712d3d74dac3741a5aa44117395709b8
elhenro <github@henry-schober.com>
2019-03-09 22:04:11 +0100

    init

---
+++ README.md
@@ -0,0 +1,33 @@
+## telemon
+
+### description
+
+a simple telegram bot that runs on your linux server to provide perfomance stats, general information and notification alerts
+
+### setup
+
+`git clone (this)`
+
+`cd telemon`
+
+get go dependencies 
+
+`go get -d ./...`
+
+place your token for the bot (obtain from botfather)
+
+`echo "1234567890123:examplePasteYourTokenHere12345" >> token.txt`
+
+run
+
+`go run main.go`
+
+### commands
+
+`hello` returns "hello world"
+
+`cpu` returns current cpu usage, read from `/proc/stat` file
+
+`ip` returns current external ip of your server
+
+more to come..
---
+++ main.go
@@ -0,0 +1,110 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	//"os/exec"
+	tb "gopkg.in/tucnak/telebot.v2"
+	"net/http"
+	"strconv"
+	"strings"
+	"time"
+)
+
+func main() {
+
+	// read token from file
+	tok, err := ioutil.ReadFile("token.txt")
+	if err != nil {
+		fmt.Print(err)
+	}
+	token := strings.TrimSpace(string(tok))
+
+	// connect with telegram bot api
+	b, err := tb.NewBot(tb.Settings{
+		Token:  token,
+		Poller: &tb.LongPoller{Timeout: 10 * time.Second},
+	})
+
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	b.Handle("hello", func(m *tb.Message) {
+		b.Send(m.Sender, "hello world")
+	})
+	b.Handle("ip", func(m *tb.Message) {
+		myExternalIp := getExternalIp()
+		b.Send(m.Sender, myExternalIp)
+	})
+	b.Handle("cpu", func(m *tb.Message) {
+		cpuUsage := getCPUUsage()
+		b.Send(m.Sender, cpuUsage)
+	})
+
+	// catchall for unknow commands
+	b.Handle(tb.OnText, func(m *tb.Message) {
+		b.Send(m.Sender, "??")
+	})
+
+	b.Start()
+}
+
+func getExternalIp() string {
+	bodyString := "(empty)"
+	resp, err := http.Get("http://ipinfo.io/ip")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode == http.StatusOK {
+		bodyBytes, err2 := ioutil.ReadAll(resp.Body)
+		if err2 != nil {
+			log.Fatal(err2)
+		}
+		bodyString = string(bodyBytes)
+		return bodyString
+	}
+	return bodyString
+}
+
+func getCPUSample() (idle, total uint64) {
+	contents, err := ioutil.ReadFile("/proc/stat")
+	if err != nil {
+		return
+	}
+	lines := strings.Split(string(contents), "\n")
+	for _, line := range lines {
+		fields := strings.Fields(line)
+		if fields[0] == "cpu" {
+			numFields := len(fields)
+			for i := 1; i < numFields; i++ {
+				val, err := strconv.ParseUint(fields[i], 10, 64)
+				if err != nil {
+					fmt.Println("Error: ", i, fields[i], err)
+				}
+				total += val // tally up all the numbers to get total ticks
+				if i == 4 {  // idle is the 5th field in the cpu line
+					idle = val
+				}
+			}
+			return
+		}
+	}
+	return
+}
+
+func getCPUUsage() string {
+	idle0, total0 := getCPUSample()
+	time.Sleep(3 * time.Second)
+	idle1, total1 := getCPUSample()
+
+	idleTicks := float64(idle1 - idle0)
+	totalTicks := float64(total1 - total0)
+	cpuUsage := 100 * (totalTicks - idleTicks) / totalTicks
+
+	return fmt.Sprintf("CPU usage is %f%% [busy: %f, total: %f]\n", cpuUsage, totalTicks-idleTicks, totalTicks)
+}
